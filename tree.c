3019b960ac0d7244b990e19534965881429a131f
theTRANCEgeek138 <ngnhatminh1205@outlook.com.vn>
2020-05-19 20:14:17 +0700

    Remove current Tree initiating function, add new alternative and fix a bug
    **Detail**
    * addChildtoLast function when executed causes Segmentation Fault due to accessing NULL pointer
    * Replace tree input algorithm, which is really inefficient

--- tree.c
+++ tree.c
@@ -9,22 +9,29 @@ typedef struct element {
 } NODE;
 
 NODE startNode;
-int maxDepth;
+
+//Quick memory allocating function for "NODE" type
+NODE *ndalloc() {
+	return (NODE*) malloc(sizeof(NODE));
+}
 
 //Add a child to to a node, next to the last existing sibling
-NODE *addChildtoLast (NODE *parentNode,int val) {
-	NODE *newNode = (NODE*) malloc(sizeof(NODE));
+NODE *addChildtoLast (NODE *targetNode,int val) {
+	//If targetNode's pointer is NULL, consider this as an error, return NULL
+	if (targetNode == NULL) return NULL;
+
+	NODE *newNode = ndalloc();
 	newNode->VALUE = val;
 	//Set newNode relatives
-	newNode->PAR = parentNode;
+	newNode->PAR = targetNode;
 	newNode->FCHD = NULL;
 	//Find the closest sibling
-	NODE *nearestSib = parentNode->FCHD;
+	NODE *nearestSib = targetNode->FCHD;
 	if (nearestSib == NULL) 
 		//If first child pointer is NULL, newNode is the first child
-		parentNode->FCHD = newNode;
+		targetNode->FCHD = newNode;
 	else {
-		while (nearestSib != NULL) nearestSib = nearestSib->NXSIB;
+		while (nearestSib->NXSIB != NULL) nearestSib = nearestSib->NXSIB;
 		nearestSib->NXSIB = newNode;
 	}
 	newNode->NXSIB = NULL;
@@ -32,10 +39,13 @@ NODE *addChildtoLast (NODE *parentNode,int val) {
 }
 
 //Add a sibling to a node, next to specified sibling node
-NODE *addSiblingtoPos (NODE *parentNode,NODE *siblingNode,int val) {
-	NODE *newNode = (NODE*) malloc(sizeof(NODE));
+NODE *addSiblingtoPos (NODE *targetNode,NODE *siblingNode,int val) {
+	//If targetNode's pointer is NULL, consider this as an error, return NULL
+	if (targetNode == NULL) return NULL;
+
+	NODE *newNode = ndalloc(); 
 	newNode->VALUE = val;
-	newNode->PAR = parentNode;
+	newNode->PAR = targetNode;
 	newNode->FCHD = NULL;
 	//Set new node relatives
 	if (siblingNode != NULL) {
@@ -43,30 +53,28 @@ NODE *addSiblingtoPos (NODE *parentNode,NODE *siblingNode,int val) {
 		siblingNode->NXSIB = newNode;
 	} else {
 		//If siblingNode pointer is NULL, it means the new node is added to 1st child position
-		newNode->NXSIB = parentNode->FCHD;
-		parentNode->FCHD = newNode;
+		newNode->NXSIB = targetNode->FCHD;
+		targetNode->FCHD = newNode;
 	}
 	return newNode;
 }
 
-//Create Tree or Sub-tree starts with specified ROOT node
-void initTree (NODE *rootNode,int currentDepth) {
-	if (currentDepth == maxDepth) return;
-	printf ("How many children: ");
-	int childrNum; scanf ("%d",&childrNum);
-	NODE *newNode = NULL;
-	//Adding "childrNum" children to the root node
+//Create child(s) starts with target Node
+void createChildr (NODE *targetNode,int childrNum) {
 	for (int i=0;i<childrNum;i++) {
-		printf ("Depth %d\nChild no.%d: ",currentDepth,i);
+		printf ("Child no.%d's value: ",i);
 		int childVal; scanf ("%d",&childVal);
-		newNode = addSiblingtoPos (rootNode,newNode,childVal);
-		initTree (newNode,currentDepth+1);
+		NODE *newNode = addChildtoLast (targetNode,childVal);
 	}
 }
 
 int main() {
-	scanf ("%d",&maxDepth);
-	initTree (&startNode,0);
+	//Initiate startNode
+	startNode.VALUE = -1;
+	startNode.FCHD = NULL;
+	startNode.PAR = NULL;
+	startNode.NXSIB = NULL;
+	
 	printf ("End of Program. Exit now\n");
 	return 0;
 }
