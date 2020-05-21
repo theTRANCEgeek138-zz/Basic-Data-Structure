1431112be4309523069f616baeb55612b574836b
theTRANCEgeek138 <ngnhatminh1205@outlook.com.vn>
2020-05-21 23:02:30 +0700

    Replace children-adding function to a parent node The new function addChildrtoNode now replaces the former createChildr function.

--- tree.c
+++ tree.c
@@ -3,16 +3,26 @@
 
 typedef struct element {
 	int VALUE;
-	struct element *FCHD;
-	struct element *NXSIB;
-	struct element *PAR;
+	struct element *FCHD; //First child Node pointer
+	struct element *NXSIB;//Next sibling Node pointer
+	struct element *PAR;  //Parent Node pointer
 } NODE;
 
-NODE startNode;
+NODE *startNode;
 
 //Quick memory allocating function for "NODE" type
 NODE *ndalloc() {
 	return (NODE*) malloc(sizeof(NODE));
+}
+
+//Create new Root node
+NODE *ndRoot(int rtVal) {
+	NODE *newRoot = ndalloc();
+	newRoot->VALUE = rtVal;
+	newRoot->PAR = NULL;
+	newRoot->NXSIB = NULL;
+	newRoot->FCHD = NULL;
+	return newRoot;
 }
 
 //Add a child to to a node, next to the last existing sibling
@@ -38,16 +48,13 @@ NODE *addChildtoLast (NODE *targetNode,int val) {
 	return newNode;
 }
 
-//Add a sibling to a node, next to specified sibling node
-NODE *addSiblingtoPos (NODE *targetNode,NODE *siblingNode,int val) {
-	//If targetNode's pointer is NULL, consider this as an error, return NULL
-	if (targetNode == NULL) return NULL;
-
+//Add a child to a node, next to specified child node
+NODE *addChildtoPos (NODE *targetNode,NODE *siblingNode,int val) {
 	NODE *newNode = ndalloc(); 
 	newNode->VALUE = val;
+	//Set new node relatives
 	newNode->PAR = targetNode;
 	newNode->FCHD = NULL;
-	//Set new node relatives
 	if (siblingNode != NULL) {
 		newNode->NXSIB = siblingNode->NXSIB;
 		siblingNode->NXSIB = newNode;
@@ -59,22 +66,27 @@ NODE *addSiblingtoPos (NODE *targetNode,NODE *siblingNode,int val) {
 	return newNode;
 }
 
-//Create child(s) starts with target Node
-void createChildr (NODE *targetNode,int childrNum) {
+NODE *addChildrtoNode (NODE *parentNode,int childrNum) {
+	NODE firstChild;
+	firstChild.NXSIB = NULL;
+	NODE *currentNode = &firstChild;
 	for (int i=0;i<childrNum;i++) {
-		printf ("Child no.%d's value: ",i);
-		int childVal; scanf ("%d",&childVal);
-		NODE *newNode = addChildtoLast (targetNode,childVal);
+		NODE *newNode = ndalloc();
+		//Input node value
+		printf ("Node no.%d: ",i);
+		int ndVal;scanf ("%d",&ndVal);
+		newNode->VALUE = ndVal;
+		newNode->PAR = parentNode;
+		currentNode->NXSIB = newNode;
+		currentNode = newNode;
 	}
+	return firstChild.NXSIB;
 }
 
 int main() {
 	//Initiate startNode
-	startNode.VALUE = -1;
-	startNode.FCHD = NULL;
-	startNode.PAR = NULL;
-	startNode.NXSIB = NULL;
-	
-	printf ("End of Program. Exit now\n");
+	startNode = NULL;
+	printf ("%d",startNode->VALUE);
+	printf ("\nEnd of Program. Exit now\n");
 	return 0;
 }
